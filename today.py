7ad945f977c9f78558c793ad9131fb4f19a29009
Sesha <seshadrivv28@gmail.com>
2026-09-16 10:46:10 -0400

    written age data and number alignment

--- today.py
+++ today.py
@@ -322,14 +322,15 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, 'commit_data', commit_data, 22)
-    justify_format(root, 'star_data', star_data, 14)
-    justify_format(root, 'repo_data', repo_data, 6)
+    justify_format(root, 'age_data', age_data)
+    justify_format(root, 'repo_data', repo_data, 20)
     justify_format(root, 'contrib_data', contrib_data)
-    justify_format(root, 'follower_data', follower_data, 10)
-    justify_format(root, 'loc_data', loc_data[2], 9)
+    justify_format(root, 'commit_data', commit_data, 25)
+    justify_format(root, 'star_data', star_data, 27)
+    justify_format(root, 'follower_data', follower_data, 23)
+    justify_format(root, 'loc_data', loc_data[2], 19)
     justify_format(root, 'loc_add', loc_data[0])
-    justify_format(root, 'loc_del', loc_data[1], 7)
+    justify_format(root, 'loc_del', loc_data[1])
     tree.write(filename, encoding='utf-8', xml_declaration=True)
 
 
